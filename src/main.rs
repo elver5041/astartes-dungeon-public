a8b15e23a5b058630b8796104b632d191f10f72e
elver5041 <joelros2003@gmail.com>
2023-02-03 17:11:35 +0100

    character creation

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,30 @@ extern crate crossterm;
 use crossterm::cursor;
 use crossterm::event::{read, Event, KeyCode, KeyEvent};
 use crossterm::style::Print;
-use crossterm::terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType};
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType, size};
 use std::io::{stdout, Write};
-
 enum ProgramScreen {
     Menu,
-    Game,
+    Game(GameStatus),
     NewGame,
     LoadGame,
-    Options,
+    Options
+}
+
+enum GameStatus {
+    Tutorial,
+    Explore,
+    Combat,
+    RandEvent,
+    StoryEvent
+}
+
+struct Character {
+    
 }
 
 fn main() {
-    let mut stdout = stdout();
+    let mut stdo = stdout();
     enable_raw_mode().unwrap();
 
     let sang_title_y = 1; //title y
@@ -33,37 +44,54 @@ fn main() {
                 let mut cursor_menu = 0;
                 let cursor_x = sang_text_x - sang_curs;
                 let cursor_y = sang_title_y + sang_text_y;
-                execute!(stdout, cursor::Hide, Clear(ClearType::All)).unwrap();
-                execute!(stdout, cursor::MoveTo(sang_title_x, sang_title_y), Print("Astares Dungeon by elver")).unwrap();
+                execute!(stdo, 
+                    cursor::Hide, 
+                    Clear(ClearType::All), 
+                    cursor::MoveTo(sang_title_x, sang_title_y), 
+                    Print("Astares Dungeon by elver ("),
+                    Print(format!("{:?}",size().unwrap())),
+                    Print(")")
+                ).unwrap();
                 let strs = ["New game", "Load game", "Options", "Quit game"];
                 for i in 0..strs.len() {
-                    execute!(stdout, cursor::MoveTo(sang_text_x, cursor_y + i as u16), Print(strs[i])).unwrap();
+                    execute!(stdo, 
+                        cursor::MoveTo(sang_text_x, cursor_y + i as u16), 
+                        Print(strs[i])
+                    ).unwrap();
                 }
                 loop {
-                    execute!(stdout, cursor::MoveTo(cursor_x, cursor_y + cursor_menu), Print(">")).unwrap();
+                    execute!(stdo, 
+                        cursor::MoveTo(cursor_x, cursor_y + cursor_menu), 
+                        Print(">"),
+                        cursor::MoveTo(cursor_x, cursor_y + cursor_menu)
+                    ).unwrap();
                     match read().unwrap() {
-                        Event::Key(KeyEvent {code: KeyCode::Up, modifiers: _ }) |
+                        Event::Key(KeyEvent {code: KeyCode::Up,        modifiers: _ }) |
                         Event::Key(KeyEvent {code: KeyCode::Char('w'), modifiers: _ }) |
                         Event::Key(KeyEvent {code: KeyCode::Char('8'), modifiers: _ }) => {
                             if cursor_menu > 0{
-                                execute!(stdout, cursor::MoveTo(cursor_x, cursor_y + cursor_menu), Print(" ")).unwrap();
+                                execute!(stdo,  
+                                    Print(" ")
+                                ).unwrap();
                                 cursor_menu -=1 ;
                             }
                         },
-                        Event::Key(KeyEvent {code: KeyCode::Down, modifiers: _ }) |
+                        Event::Key(KeyEvent {code: KeyCode::Down,      modifiers: _ }) |
                         Event::Key(KeyEvent {code: KeyCode::Char('s'), modifiers: _ }) |
                         Event::Key(KeyEvent {code: KeyCode::Char('2'), modifiers: _ }) => {
                             if cursor_menu + 1 < strs.len() as u16 {
-                                execute!(stdout, cursor::MoveTo(cursor_x, cursor_y + cursor_menu), Print(" ")).unwrap();
+                                execute!(stdo, 
+                                    Print(" ")
+                                ).unwrap();
                                 cursor_menu += 1;
                             }
                         },
-                        Event::Key(KeyEvent{code: KeyCode::Enter, modifiers: _}) |
+                        Event::Key(KeyEvent{code: KeyCode::Enter,     modifiers: _}) |
                         Event::Key(KeyEvent{code: KeyCode::Char(' '), modifiers: _}) => {
                             match cursor_menu {
                                 0 => program_screen = ProgramScreen::NewGame,
                                 1 => program_screen = ProgramScreen::LoadGame,
-                                2 => program_screen = ProgramScreen::Options,
+                                2 => program_screen = ProgramScreen::Menu,
                                 3 => break 'main,
                                 _ => ()
                             };
@@ -73,25 +101,38 @@ fn main() {
                     };
                 }
             },
-            ProgramScreen::Game => {
+            ProgramScreen::Game(game_status) => {
                 todo!()
             },
             ProgramScreen::NewGame => {
-                execute!(stdout, cursor::Hide, Clear(ClearType::All)).unwrap();
-                execute!(stdout, cursor::MoveTo(1, 1), Print("game made")).unwrap();
-                program_screen = ProgramScreen::Game;
+                execute!(stdo, 
+                    cursor::Hide, 
+                    Clear(ClearType::All), 
+                    cursor::MoveTo(1, 1), 
+                    Print("game made")
+                ).unwrap();
+                program_screen = ProgramScreen::Game(GameStatus::Tutorial);
             },
             ProgramScreen::LoadGame => {
-                execute!(stdout, cursor::Hide, Clear(ClearType::All)).unwrap();
-                execute!(stdout, cursor::MoveTo(1, 1), Print("game load")).unwrap();
-                program_screen = ProgramScreen::Game;
+                execute!(stdo, 
+                    cursor::Hide, 
+                    Clear(ClearType::All), 
+                    cursor::MoveTo(1, 1), 
+                    Print("game load")
+                ).unwrap();
+                program_screen = ProgramScreen::Game(GameStatus::Explore);
             },
             ProgramScreen::Options => {
                 todo!()
             }
         }
     }
-    execute!(stdout, cursor::Hide, Clear(ClearType::All)).unwrap();
-    execute!(stdout, cursor::MoveTo(1, 1), Print("Thanks for playing")).unwrap();
+    execute!(stdo, 
+        cursor::Hide, 
+        Clear(ClearType::All), 
+        cursor::MoveTo(1, 1), 
+        Print("Thanks for playing"),
+        cursor::MoveTo(0, 4)
+    ).unwrap();
     disable_raw_mode().unwrap();
 }
