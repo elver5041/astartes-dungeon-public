04391d8002eab748bee9c6b7b386dd311285809d
elver5041 <joelros2003@gmail.com>
2023-02-04 12:21:15 +0100

    character reform

--- src/character.rs
+++ src/character.rs
@@ -1,11 +1,3 @@
-pub struct Stats {
-    str: u8,
-    def: u8,
-    agi: u8,
-    lib: u8,
-    corr: u8
-}
-
 pub enum Size {
     Nonexistent,
     Small(u8), //0-10
@@ -19,18 +11,6 @@ pub enum Gender {
     Female,
     NonBinary
 }
-pub struct Appearance {
-    height: u16,
-    weight: u16,
-    age: u8,
-    gender: Gender,
-    gender_app: i8,
-    breasts: Option<Size>,
-    butt: Option<Size>,
-    penis: Option<Size>,
-    vagina: Option<Size>,
-    anus: Option<Size>
-}
 
 pub enum Species {
     Human,
@@ -38,48 +18,75 @@ pub enum Species {
     Goblin,
     Succubus
 }
-
-pub struct Character {
-    name: String,
+pub struct Stats {
+    lvl: u8,
     max_hp: u32,
     hp: u32,
     max_mp: u32,
     mp: u32,
+    corr: u8,
+}
+
+pub struct Attributes {
+    str: u8,
+    def: u8,
+    agi: u8,
+    lib: u8
+}
+
+pub struct Appearance {
+    height: u16,
+    weight: u16,
+    age: u8,
+    gender_app: i8,
+    breasts: Option<Size>,
+    butt: Option<Size>,
+    penis: Option<Size>,
+    vagina: Option<Size>,
+    anus: Option<Size>,
+    species: (Species, Option<Species>)
+}
+
+pub struct Character {
+    name: String,
+    gender: Gender,
     stats: Stats,
-    appearance: Appearance,
-    species: Species,
-    hybrid: Option<Species>
+    attributes: Attributes,
+    appearance: Appearance
 }
 
 impl Character {
     pub fn new() -> Character {
         Character{
             name: String::from("Protagonist"),
-            max_hp: 1,
-            hp: 1,
-            max_mp: 1,
-            mp: 1,
+            gender: Gender::Male,
             stats: Stats{
+                lvl: 1,
+                max_hp: 1,
+                hp: 1,
+                max_mp: 1,
+                mp: 1,
+                corr: 0
+            },
+            attributes: Attributes{
                 str: 1,
                 def: 1,
                 agi: 1,
                 lib: 1,
-                corr: 0,
             },
             appearance: Appearance{
                 height: 170,
                 weight: 500,
                 age: 20,
-                gender: Gender::Male,
                 gender_app: 100,
                 breasts: None,
                 butt: Some(Size::Small(5)),
                 penis: Some(Size::Medium(15)),
                 vagina: None,
                 anus: Some(Size::Small(5)),
-            },
-            species: Species::Human,
-            hybrid: None,
+                species: (Species::Human, None)
+            }
         }
     }
 }
+
